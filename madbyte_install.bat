b090dbbbba16a951d36e469138da082206d330b1
jvansan <jeffreyavansanten@gmail.com>
2020-11-23 15:36:37 -0800

    fixes: batch install and exec scripts

--- madbyte_install.bat
+++ madbyte_install.bat
@@ -1,9 +1,26 @@
 @echo on
-set root=%LocalAppData%\Continuum\anaconda3
-call %root%\Scripts\activate.bat
 
-echo "Setting up virtual environment and installing dependencies for MADByTE!"
+set root1=%userprofile%\anaconda3
+set root2=%LocalAppData%\Continuum\anaconda3
+
+IF EXIST %root1% (
+echo Anaconda Python found!
+call %root1%\Scripts\activate.bat
+) ELSE (
+IF EXIST %root2% (
+echo Anaconda Python found!
+call %root2%\Scripts\activate.bat
+) ELSE (
+echo Anaconda Python could not be found!
+echo Please make sure you have Anaconda installed in the default location or report and issue to the MADByTE team.
+TIMEOUT 10
+exit /b
+)
+)
+
+
+echo Setting up virtual environment and installing dependencies for MADByTE!
 call conda env create -f environment.yml
-echo "Done installing MADByTE!"
-pause
+echo Done installing MADByTE!
+TIMEOUT 10
 
